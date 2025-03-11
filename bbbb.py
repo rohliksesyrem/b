0fb07d68e97678b7fdbe084767242dd32dbd7192
unknown <jiri.rzyman@infis.cz>
2025-03-11 09:33:58 +0100

    1

---
+++ bbbb.py
@@ -0,0 +1,20 @@
+bubu = input("HEEEJ")
+## zeptat se a načíst proměnou
+cislo = 1
+int(cislo)
+##přetipování
+if cislo >= 5:
+    print("NEGR")
+elif cislo < 5:
+    print("albin negr")
+else:
+    print("neni cislo")
+##podminky 
+print("OVCE")
+##neco vypise 
+cislo > 5 and cislo <6
+# logicky ne asi
+
+# git add *
+# git commit -m
+# git push
